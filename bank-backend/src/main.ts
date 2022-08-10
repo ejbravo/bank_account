43475b9837eaea01db9af086892efb438d047570
Eduardo Bravo <1002456@ergo.com>
2022-08-10 18:23:13 +0200

    Environment variables added. ENV file created.

--- bank-backend/src/main.ts
+++ bank-backend/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT ? parseInt(process.env.PORT) : 3001;
+  console.log(port);
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
