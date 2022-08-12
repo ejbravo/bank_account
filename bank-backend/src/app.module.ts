576843c429a50ac4152daeb2aa7a448184d1f416
Eduardo Bravo <1002456@ergo.com>
2022-08-12 17:54:05 +0200

    postgres connection done

--- bank-backend/src/app.module.ts
+++ bank-backend/src/app.module.ts
@@ -3,9 +3,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovementsModule } from './movements/movements.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MovementsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MovementsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'bank-movements',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
