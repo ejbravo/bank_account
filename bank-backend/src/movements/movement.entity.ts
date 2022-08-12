6a9c54616b8c0bcccfda249d674b54e7e541ee1e
Eduardo Bravo <1002456@ergo.com>
2022-08-12 17:58:22 +0200

    Movement from interface to Entity with typeorm

--- bank-backend/src/movements/movement.entity.ts
+++ bank-backend/src/movements/movement.entity.ts
@@ -1,12 +1,31 @@
-export interface Movement {
-  id: string;
-  date: number;
-  type: MovementType;
-  amount: number;
-  balance: number;
-}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+// export interface Movement {
+//   id: string;
+//   date: number;
+//   type: MovementType;
+//   amount: number;
+//   balance: number;
+// }
 
 export enum MovementType {
   INCOME = 'income',
   WITHDRAW = 'withdraw',
+}
+@Entity()
+export class Movement {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  date: number;
+
+  @Column()
+  type: MovementType;
+
+  @Column()
+  amount: number;
+
+  @Column()
+  balance: number;
 }
