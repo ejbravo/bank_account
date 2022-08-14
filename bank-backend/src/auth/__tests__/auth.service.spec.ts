8dfe931e9f002d703a3d5734905f1b1d4a845914
Eduardo Bravo <1002456@ergo.com>
2022-08-14 12:19:01 +0200

    Tested the method createUser of AuthService

--- bank-backend/src/auth/__tests__/auth.service.spec.ts
+++ bank-backend/src/auth/__tests__/auth.service.spec.ts
@@ -2,10 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthService } from '../auth.service';
+import { AuthDto } from '../dto/auth.dto';
 import { User } from '../user.entity';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
   let repository: Repository<User>;
 
   beforeEach(async () => {
@@ -19,11 +20,35 @@ describe('AuthService', () => {
       ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
     repository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('Should create an user if everything is ok', async () => {
+    const mockCardId = '1234567812345678';
+    const mockPin = '1234';
+
+    const mockAuthDto: AuthDto = {
+      cardId: mockCardId,
+      pin: mockPin,
+    };
+    const mockUser: User = {
+      id: '1234',
+      cardId: mockCardId,
+      pin: mockPin,
+      balance: 0,
+    };
+
+    jest.spyOn(repository, 'create').mockReturnValue(mockUser);
+    jest.spyOn(repository, 'save').mockResolvedValue(mockUser);
+
+    await authService.createUser(mockAuthDto);
+
+    expect(repository.create).toHaveBeenCalled();
+    expect(repository.save).toBeCalledTimes(1);
   });
 });
